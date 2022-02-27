d136cbd3e79deb30d63b86c2e504271680af1328
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-27 20:12:30 -0300

    Project tcp_client

--- tcp_client/tcp_client.py
+++ tcp_client/tcp_client.py
@@ -1,2 +1,30 @@
 import socket
 import sys
+
+def main():
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+	except socket.error as err:
+		print("Connection failed!")
+		print("Error: {}".format(err))
+		sys.exit()
+
+
+	print("Success")
+
+
+	host = input("Target address: ")
+	port = input("Target port: ")
+
+	try:
+		s.connect((host, int(port)))
+		print("Connection Success")
+		s.shutdown(2)
+	except socket.error as err:
+		print("Failed!")
+		print("Error: {}".format(err))
+		sys.exit()
+
+
+if __name__ == "__main__":
+	main()
